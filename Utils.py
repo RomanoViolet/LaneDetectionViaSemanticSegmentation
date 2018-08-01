830c8883203497f88791028d5b40153e359ebad7
RomanoViolet <19875252+RomanoViolet@users.noreply.github.com>
2018-08-01 21:26:57 +0200

    Cleanup: Removed downloadable input dependencies

--- Utils.py
+++ Utils.py
@@ -11,6 +11,7 @@ import zipfile
 
 import Configuration
 
+
 # Download progress tracker
 class DLProgress(tqdm):
     last_block = 0
@@ -22,28 +23,32 @@ class DLProgress(tqdm):
 
 
 def verifyPythonVersion():
-    assert sys.version_info >= (3, 3), "You require Python v. 3.3 or newer. You are using {a:s}".format(a=python_version())
+    assert sys.version_info >= (3, 3), "You require Python v. 3.3 or newer. You are using {a:s}".format(
+        a=python_version())
 
 
 # Utility to detect tensorflow version, and assert if the version is older than 1.0
 def verifyTensorFlowVersion():
-    assert LooseVersion(tf.__version__) >= LooseVersion('1.0'), 'Please use TensorFlow version 1.0 or newer.  You are using {}'.format(tf.__version__)
-
+    assert LooseVersion(tf.__version__) >= LooseVersion(
+        '1.0'), 'Please use TensorFlow version 1.0 or newer.  You are using {}'.format(tf.__version__)
 
 
 def issueNoGPUWarning():
     if not tf.test.gpu_device_name():
-        warnings.warn('No GPU found. Please use a GPU to train your neural network. Training on a CPU maybe 10x or more slower.')
+        warnings.warn(
+            'No GPU found. Please use a GPU to train your neural network. Training on a CPU maybe 10x or more slower.')
     else:
         print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
 
 
 def isKittiDataSetAvailable():
     if (not os.path.isdir(os.path.join(Configuration.PathtoKITTIDataSet, "data_road"))):
-        print("Kitti data set is not available. Expected a folder 'data_road' at location {a:s}. Download from: {b:s}".format(a=os.path.join(Configuration.PathtoKITTIDataSet, "data_road"), b='http://www.cvlibs.net/datasets/kitti/eval_road.php'))
+        print(
+            "Kitti data set is not available. Expected a folder 'data_road' at location {a:s}. Download from: {b:s}, and thereafter Extract the dataset in the \'KittiDataSet\' folder. This will create the subfolder \'data_road\' with all the training and test images".format(
+                a=os.path.join(Configuration.PathtoKITTIDataSet, "data_road"),
+                b='http://www.cvlibs.net/datasets/kitti/eval_road.php'))
         sys.stdout.flush()
         assert False, "Kitti data set is not available."
-
 
 
 # Download the VGG model if not already available locally
@@ -87,7 +92,8 @@ def downloadVGGIfNotLocallyAvailable():
 # Check whether the augmented VGG model is availbale for use
 def isAugmentedVGGModelAvailable():
     if (not os.path.isfile(os.path.join(Configuration.PathtoSaveTrainedAugmentedModel, "saved_model.pb"))):
-        print("Augmented VGG Model not available. Run Training first. Expected a file 'saved_model.pb' at location {a:s}".format(a=os.path.join(Configuration.PathtoSaveTrainedAugmentedModel, "saved_model.pb")))
+        print(
+            "Augmented VGG Model not available. Run Training first. Expected a file 'saved_model.pb' at location {a:s}".format(
+                a=os.path.join(Configuration.PathtoSaveTrainedAugmentedModel, "saved_model.pb")))
         sys.stdout.flush()
         assert False, "Augmented VGG Model not available."
-
